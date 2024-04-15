1cda88ec4965377489bdcf87ab61f1b8d7d1b461
Manuel Westermeier <150041048+ManuelWestermeier@users.noreply.github.com>
2024-04-15 16:16:17 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,24 @@
-if (location.port != 5173) {
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v2");
+    await cache.addAll(resources);
+};
 
-    const addResourcesToCache = async (resources) => {
-        const cache = await caches.open("v2");
-        await cache.addAll(resources);
-    };
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        addResourcesToCache([
+            "/",
+            "/index.html",
+            "/assets/index-8HvtRm0o.js",
+            "/assets/index-DnbXvAQW.css",
+            "/logo.jpg",
+            "/manifest.json",
+            "/service-worker.js"
+        ]),
+    );
+});
 
-    self.addEventListener("install", (event) => {
-        event.waitUntil(
-            addResourcesToCache([
-                "/",
-                "/index.html",
-                "/assets/index-8HvtRm0o.js",
-                "/assets/index-DnbXvAQW.css",
-                "/logo.jpg",
-                "/manifest.json",
-                "/service-worker.js"
-            ]),
-        );
-    });
-
-    self.addEventListener("fetch", e => {
-        e.respondWith(
-            fetch(e.request).catch(() => caches.match(e.request))
-        )
-    })
-
-}
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
+    )
+})
